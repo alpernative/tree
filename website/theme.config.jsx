89b9c1d3c08a965fd8b5976662e594920023c6f2
alperenkaraguzel <alperenkaraguzel@gmail.com>
2024-02-12 15:43:57 +0300

    feat:(nextra): nextra implementation

---
+++ website/theme.config.jsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+
+const StyledLogo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: 600;
+  font-size: 25px;
+  cursor: pointer;
+`;
+
+const StyledImage = styled(Image)`
+  border-radius: 10px;
+`;
+
+export default {
+  logo: (
+    <StyledLogo>
+      <StyledImage src="/logo.png" width={40} height={40} />
+      <span>Alper Native</span>
+    </StyledLogo>
+  ),
+  docsRepositoryBase: 'https://github.com/alpernative/tree/tree/master/website',
+  project: {
+    link: 'https://github.com/alpernative/tree',
+  },
+  // ... other theme options
+};
